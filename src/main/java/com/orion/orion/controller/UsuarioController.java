c4cb7099319d1ff1b16d0104800b5dab2b9f157b
Guilherme Henrique de Sousa Jesus <guilherme.devsec@icloud.com>
2025-09-10 21:06:48 -0300

    Inserção de Constructor DTO, Metodos Utilizando Optional e List

--- src/main/java/com/orion/orion/controller/UsuarioController.java
+++ src/main/java/com/orion/orion/controller/UsuarioController.java
@@ -13,6 +13,8 @@ import com.orion.orion.service.UsuarioService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -24,21 +26,32 @@ public class UsuarioController {
 
     @GetMapping("/id/{id_usuario}")
     
+    //Optional Representa um Unico valor que pode ou não existir - quando não tem certeza se existe objeto 
     public ResponseEntity<Optional<UsuarioDTO>> getMethodName(
                                                                 @PathVariable(name = "id_usuario") Integer id_usuario) {
             Optional<UsuarioEntity> usuario = usuarioService.chamaUsuario(id_usuario);  
-            Optional<UsuarioDTO> dtos = usuario.stream()
-                            .map(UsuarioDTO::new)    
-                            .toList();
+            //retirado stream e mantido Map no Optional 
+            Optional<UsuarioDTO> dtos = usuario.map(UsuarioDTO::new);    
 
             return ResponseEntity.ok(dtos)                ;
 
     }
 
+    //Exemplo do login em Dto Utilizando a List = List usada Varios resultados ou nenhum
     @GetMapping("/login/{login}")
+    public ResponseEntity<List<UsuarioDTO>> getMethodName(@PathVariable String login) {
+        List<UsuarioEntity> entity = usuarioService.chamaLogin(login);
+        List<UsuarioDTO> dtos = entity.stream()
+                                      .map(UsuarioDTO::new)
+                                      .toList();
+        return ResponseEntity.ok(dtos);
+    }
+    
+
+    /*@GetMapping("/login/{login}")
     public List<UsuarioEntity> getMethodLogin(@PathVariable(name = "login") String login) {
         return usuarioService.chamaLogin(login);
-    }
+    }*/
 
       
 
